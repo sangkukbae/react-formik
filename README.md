fe622222dcb2064d0f60b358c46c26bf7fc727b8
Sangkuk Bea <sangkukbea@naver.com>
2020-03-08 22:55:26 +0900

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-formik
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import SignupForm from "./SignupComponent";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>formik</h1>
+      <SignupForm />
+    </div>
+  );
+}
